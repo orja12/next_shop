05e4b26cf39f89c3bb04d6a992bc030a76d5c3b4
orja12 <alrudaini.omar@outlook.com>
2025-12-04 18:39:17 +0000

    Auto-update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-
 export default function Home(){
   return(
     <main style={{padding:"40px",textAlign:"center"}}>
-      <h1 style={{fontSize:"32px",fontWeight:"bold", color: "#fff"}}>مرحباً بك في متجرك الإلكتروني</h1>
+      <h1 style={{fontSize:"32px",fontWeight:"bold", color: "#fff"}}>متجر نكست جي إس</h1>
       <p>سويلي متجر Next.js جديد</p>
     </main>
   );
