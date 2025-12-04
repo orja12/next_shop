5347cc139b0efd2af5d0536298218a767a26d83f
orja12 <alrudaini.omar@outlook.com>
2025-12-04 03:59:47 +0000

    Auto-update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home(){
   return(
     <main style={{padding:"40px",textAlign:"center"}}>
-      <h1 style={{fontSize:"32px",fontWeight:"bold"}}>Next.js جاهز</h1>
+      <h1 style={{fontSize:"32px",fontWeight:"bold", color: "#000"}}>Next.js جاهز</h1>
       <p>سويلي متجر Next.js جديد</p>
     </main>
   );
