cc521a10d2d0ba8757bfa0e1482bc57f408040ac
orja12 <alrudaini.omar@outlook.com>
2025-12-04 03:17:34 +0100

    Add file

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home(){
+  return(
+    <main style={{padding:"40px",textAlign:"center"}}>
+      <h1 style={{fontSize:"32px",fontWeight:"bold"}}>Next.js جاهز</h1>
+      <p>سويلي متجر Next.js جديد</p>
+    </main>
+  );
+}
